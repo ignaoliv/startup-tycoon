26315d1b0415e18bcc39d9016d7140079ba3e2a4
Ignacio Olivieri <dji.olivieri@gmail.com>
2026-09-03 18:52:31 -0300

    Permitir abrir el dev server desde la red local

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // permite abrir el dev server desde el celu en la misma red
+  allowedDevOrigins: ["192.168.1.70", "192.168.1.*", "*.local"],
 };
 
 export default nextConfig;
